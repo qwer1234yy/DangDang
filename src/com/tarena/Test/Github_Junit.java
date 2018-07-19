2511d9e3d0c7df62d88dd0b059bfdd96a0ec21af
Robbie <583198238@qq.com>
2018-07-19 16:20:08 +0800

    Github test child branch

---
+++ src/com/tarena/Test/Github_Junit.java
@@ -0,0 +1,14 @@
+package com.tarena.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Github_Junit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
